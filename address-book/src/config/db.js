34b1702a52d607b90da58548c0eac6d058380a69
Andrew David <56502440+andydavidlee@users.noreply.github.com>
2020-11-05 08:58:12 +1100

    08:58

---
+++ address-book/src/config/db.js
@@ -0,0 +1,14 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyATIsCPyS-fwHNeSg9npUhrNgNBJF_DN3g",
+    authDomain: "addressbook-5a4a4.firebaseapp.com",
+    databaseURL: "https://addressbook-5a4a4.firebaseio.com",
+    projectId: "addressbook-5a4a4",
+    storageBucket: "addressbook-5a4a4.appspot.com",
+    messagingSenderId: "782769281776",
+    appId: "1:782769281776:web:f326bef4a8287392348035"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
